118ac5df0fff3912d774ea2fd852f93b132ba9a5
taliirafael <154661297+taliirafael@users.noreply.github.com>
2024-01-10 03:25:18 +1100

    Add files via upload
    attempt 1

--- LowerCase.java
+++ LowerCase.java
@@ -12,6 +12,15 @@ public class LowerCase {
     */
     public static String lowerCase(String s) {
         // Replace the following statement with your code
-        return null;
+        String lowerCaseString = "";
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) >= 65 && s.charAt(i) <= 90) {
+                    lowerCaseString += (char)(s.charAt(i) + 32);
+                }
+            else {
+                        lowerCaseString += s.charAt(i);
+                }
+            }
+        return lowerCaseString;
     }
 }
